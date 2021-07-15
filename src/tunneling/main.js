91fa08a5cb67909c153324ad0fe763c483511821
밋꾸 <51738555+dndnp4@users.noreply.github.com>
2021-07-15 16:08:45 +0900

    Create main.js

---
+++ src/tunneling/main.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const mysql = require('mysql');
+const path = require('path');
+const tunnel = require('tunnel-ssh');
+
+const key = fs.readFileSync(path.join(__dirname, './key.ppk'));
+
+const tunnelConfig = {
+    username : 'name',
+    host : 'host',
+    port : 22,
+    privateKey : key,
+    dstHost : '터널뚫을곳 호스트',
+    dstPort : 3306, //뚫을곳 포트
+    localPort : 3308    // 뚫을곳과 연결할 로컬 포트
+}
+const mysqlConfig = {
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'root',
+    port : 3308,
+    database : 'db',
+    connectionLimit : 100,
+    multipleStatements :true,
+    waitForConnections : true,
+    dateStrings : 'date'
+}
+
+const pool = mysql.createPool(mysqlConfig);
+
+function getConnection(){
+    return new Promise(function(resolve, reject){
+        try {
+            pool.getConnection(function(err, conn){
+                if(err) throw e;
+                return resolve({
+                    query : function(sql, params){
+                        return new Promise(function(mResolve, mReject){
+                            conn.query(sql, params, function(error, rows){
+                                if(error) return mReject(error);
+                                return mResolve(rows);
+                            });
+                        });
+                    },
+                    beginTransaction : function(){
+                        return new Promise(function(mResolve, mReject){
+                            conn.beginTransaction(function(error){
+                                if(error) return mReject(error);
+                                return mResolve();
+                            })
+                        });
+                    },
+                    rollback : function(){
+                        return new Promise(function(mResolve, mReject){
+                            conn.rollback(function(error){
+                                if(error) return mReject(error);
+                                return mResolve();
+                            })
+                        });
+                    },
+                    commit : function(){
+                        return new Promise(function(mResolve, mReject){
+                            conn.commit(function(error){
+                                if(error) return mReject(error);
+                                return mResolve();
+                            })
+                        });
+                    },
+                    release : function(){
+                        conn.release();
+                    }
+                });
+            });
+        }catch(e) {
+            return reject(e);
+        }
+    });
+}
+
+tunnel(tunnelConfig, async function(error, server){
+    const conn1 = await getConnection();
+    const data1 = await conn1.query(`select * from user limit 1;`);
+    conn1.release();
+    //TODO ............
+});
